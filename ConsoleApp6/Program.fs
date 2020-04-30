af42b8df405a43e0fca76340dde5fb7b9b4cfc29
Yonatan Reicher <yony252525@outlook.com>
2020-04-30 14:56:43 +0300

    Changed the main again so that it does not run the code immidietly but does compile it

--- ConsoleApp6/Program.fs
+++ ConsoleApp6/Program.fs
@@ -14,7 +14,7 @@ open Asmb.IL.Write
 open Asmb.Translate
 
 /// Compiles asmb code from a given source file from the TASM folder. fileName is the name of the file in th TASM folder 
-let compileAndRun debug (optimize: bool) (fileName: string) =
+let compile (optimize: bool) (fileName: string) =
     let fileName = if fileName.EndsWith ".ab" then fileName.[0..fileName.Length-1-3] else fileName
     let sourceCode = File.ReadAllText (sprintf @"C:\TASM\%s.ab" fileName)
     match run pprogram sourceCode with
@@ -24,7 +24,7 @@ let compileAndRun debug (optimize: bool) (fileName: string) =
         let str = writeProg program     
         printfn "%s" str
         File.WriteAllText(fileName + ".asm", str)
-        let p = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe", sprintf @"-c ""TASM %s"" -c ""TLINK %s"" -c ""%s""" (fileName + ".asm") fileName (if debug then "TD " + fileName else fileName))
+        let p = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe", sprintf @"-c ""TASM %s"" -c ""TLINK %s""" (fileName + ".asm") fileName)
         p.WaitForExit()
         p.Dispose()
         
@@ -33,10 +33,8 @@ let compileAndRun debug (optimize: bool) (fileName: string) =
 [<EntryPoint>]
 let rec main args = 
     match args with
-    | [|fileName; "-debug"|] ->
-        compileAndRun true true fileName
     | [|fileName|] ->
-        compileAndRun false true fileName
-    | _ -> printfn "Incorrect arguments! \nUse: asmb file-name OR asmb file-name -debug"
+        compile true fileName
+    | _ -> printfn "Incorrect arguments! \nUse: asmb file-name"
     Console.ReadKey true |> ignore
     0
