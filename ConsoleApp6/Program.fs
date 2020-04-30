7d0b99167f9cee22d1777f8687377ffbf89b64d2
Yonatan Reicher <yony252525@outlook.com>
2020-04-30 14:48:42 +0300

    Changed main function to call dosbox directly and compile and then run the program immidiatly

--- ConsoleApp6/Program.fs
+++ ConsoleApp6/Program.fs
@@ -1,6 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Diagnostics
+open System.IO
 
 open FParsec
 open Asmb
@@ -11,61 +13,30 @@ open Asmb.IL.Optimization
 open Asmb.IL.Write
 open Asmb.Translate
 
-let handleSourceCode = 
-    run pprogram
-    >>  function
-        | Success (a,(),_) ->
-            let b = 
-                a
-                |> translateProg
-                |> optimizeProgram
-                |> writeProg
-                |> fun s -> s.Replace("\t", "    ")
-            printfn "Compilation done successfuly!"
-            printfn "Would you like to see dump? \n1 - tree \n2 - file \n3 - both \nother - none"
-            match Console.ReadLine () with
-            | "1" -> printfn "Tree: \n%O" a
-            | "2" -> printfn "File: \n%s" b
-            | "3" -> printfn "Tree: \n%O \n\nFile: \n%s" a b
-            | _ -> printfn "Fine! enjoy your .asm file"
-        | Failure (a,b,c) ->
-            printfn "%O \n%O \n%O" a b c
+/// Compiles asmb code from a given source file from the TASM folder. fileName is the name of the file in th TASM folder 
+let compileAndRun debug (optimize: bool) (fileName: string) =
+    let fileName = if fileName.EndsWith ".ab" then fileName.[0..fileName.Length-1-3] else fileName
+    let sourceCode = File.ReadAllText (sprintf @"C:\TASM\%s.ab" fileName)
+    match run pprogram sourceCode with
+    | Failure (error, _, _) -> printfn "%s" error
+    | Success (asmbProgram, (), _) ->
+        let program = translateProg asmbProgram |> if optimize then optimizeProgram else id
+        let str = writeProg program     
+        printfn "%s" str
+        File.WriteAllText(fileName + ".asm", str)
+        let p = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe", sprintf @"-c ""TASM %s"" -c ""TLINK %s"" -c ""%s""" (fileName + ".asm") fileName (if debug then "TD " + fileName else fileName))
+        p.WaitForExit()
+        p.Dispose()
+        
+//let runAssembly
 
 [<EntryPoint>]
 let rec main args = 
     match args with
-    | [||] ->
-        printfn "File name or 2:dont or 3:awesome"
-        let fileName = Console.ReadLine ()
-        match fileName with
-        | "dont" | "2" ->
-            let rec readLines () = let line = Console.ReadLine () in if line = ";;" then "" else sprintf "%s\n%s" line (readLines ())
-            printfn "Fine. Then input your source code:"
-            readLines ()
-            |> handleSourceCode
-        | "awesome" | "3" ->
-            let code = @"
-func word main() {
-    return fib(12);
-}
-
-func word fib(x word) { 
-    if x = 1 {
-        return 0;
-    } else {
-        if x = 2 {
-            return 1;
-        } else {
-            return fib(x - 1) + fib(x - 2);
-        };   
-    };
-}"
-            printfn "Code: \n%s" code
-            handleSourceCode code
-        | _ -> ignore(main [|fileName|])
+    | [|fileName; "-debug"|] ->
+        compileAndRun true true fileName
     | [|fileName|] ->
-        let fileName = if fileName.EndsWith ".ab" then fileName.[0..fileName.Length-1-3] else fileName  //  Trim the extension
-        IO.File.ReadAllText (sprintf "./%s.ab" fileName)
-        |> handleSourceCode
+        compileAndRun false true fileName
+    | _ -> printfn "Incorrect arguments! \nUse: asmb file-name OR asmb file-name -debug"
     Console.ReadKey true |> ignore
     0
