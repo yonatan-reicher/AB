0e544d6789e2d3edff63b6bfbafb681e83daa00e
Yonatan Reicher <yony252525@outlook.com>
2020-05-26 21:19:14 +0300

    Added interop for closing dosbox subprocess automatically

--- ConsoleApp6/Program.fs
+++ ConsoleApp6/Program.fs
@@ -1,17 +1,49 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.Diagnostics
 open System.IO
 
 open FParsec
-open Asmb
-open Asmb.AST
 open Asmb.AST.Parsing
-open Asmb.IL
 open Asmb.IL.Optimization
 open Asmb.IL.Write
 open Asmb.Translate
+
+module Interop = 
+    open System.Runtime.InteropServices
+
+    type CtrlType =
+        | CTRL_C_EVENT = 0
+        | CTRL_BREAK_EVENT = 1
+        | CTRL_CLOSE_EVENT = 2
+        | CTRL_LOGOFF_EVENT = 5
+        | CTRL_SHUTDOWN_EVENT = 6
+
+    // https://msdn.microsoft.com/fr-fr/library/windows/desktop/ms683242.aspx
+    type SetConsoleCtrlEventHandler = delegate of CtrlType -> bool
+
+    // https://msdn.microsoft.com/fr-fr/library/windows/desktop/ms686016.aspx
+    [<DllImport("Kernel32")>]
+    extern bool SetConsoleCtrlHandler(SetConsoleCtrlEventHandler handler, bool add)
+
+    let mutable private cleanUp = []
+    let handler: SetConsoleCtrlEventHandler =
+        //SetConsoleCtrlEventHandler(function
+        //    | CtrlType.CTRL_BREAK_EVENT
+        //    | CtrlType.CTRL_C_EVENT
+        //    | CtrlType.CTRL_LOGOFF_EVENT
+        //    | CtrlType.CTRL_SHUTDOWN_EVENT
+        //    | CtrlType.CTRL_CLOSE_EVENT ->
+        //        // TODO Cleanup resources
+        //        for c in cleanUp do c()
+        //        Environment.Exit(0);
+        //        false
+        //    | _ -> false)
+        SetConsoleCtrlEventHandler(fun _ -> (for c in cleanUp do c()); false)
+    let onExit x = cleanUp <- x :: cleanUp
+
+    do
+        SetConsoleCtrlHandler(handler, true) |> ignore
+
 
 /// Compiles asmb code from a given source file from the TASM folder. fileName is the name of the file in th TASM folder 
 let compile (optimize: bool) (fileName: string) =
@@ -20,15 +52,14 @@ let compile (optimize: bool) (fileName: string) =
     match run pprogram sourceCode with
     | Failure (error, _, _) -> printfn "%s" error
     | Success (asmbProgram, (), _) ->
-        let program = translateProg asmbProgram |> if optimize then optimizeProgram else id
+        let program = translateProgram asmbProgram |> if optimize then optimizeProgram else id
         let str = writeProg program     
         printfn "%s" str
         File.WriteAllText(sprintf "C:\TASM\%s.asm" fileName, str)
-        let p = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe", sprintf @"-c ""TASM %s"" -c ""TLINK %s""" fileName fileName)
-        p.WaitForExit()
-        p.Dispose()
         
-//let runAssembly
+        use p = System.Diagnostics.Process.Start(@"C:\Program Files (x86)\DOSBox-0.74-3\DOSBox.exe", sprintf @"-c ""TASM %s"" -c ""TLINK %s""" fileName fileName)
+        Interop.onExit p.Kill
+        p.WaitForExit()
 
 [<EntryPoint>]
 let rec main args = 
